01d9e1889d04788ecc5e68cb899905a0179c85e7
VladislavJ <dkfl123@bigmir.net>
2016-03-12 18:00:12 +0200

    123
    123

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+	postcss = require('gulp-postcss');
+	rename = require("gulp-rename");
+
+gulp.task('css', function () {
+return gulp.src('./css/main.css')
+.pipe(postcss([
+	require('postcss-import'),
+	require('postcss-nested'),
+	require('autoprefixer'),
+]))
+.pipe(rename('./bundle.css'))
+.pipe(gulp.dest('./css/'))
+});
+
+gulp.task('watch',function () {
+	gulp.watch('./css/*.css', ['css'])
+})
+
+
+
+
